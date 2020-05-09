33b7eae24ad2cfe3c438c550e2bb80d1dbf2f032
Greg Kostal <gkostal@microsoft.com>
2020-05-09 17:27:02 +0000

    Switch to shared attestation provider in US West

--- sgx.attest.sample/validatequotes/runall.sh
+++ sgx.attest.sample/validatequotes/runall.sh
@@ -2,8 +2,8 @@
 # 
 # Script to verify all example remote attestation quotes
 #
-dotnet run ../genquotes/quotes/enclave.info.debug.json              tradewinds.us.test.attest.azure.net   false
-dotnet run ../genquotes/quotes/enclave.info.release.json            tradewinds.us.test.attest.azure.net   false
-dotnet run ../genquotes/quotes/enclave.info.prodid.json             tradewinds.us.test.attest.azure.net   false
-dotnet run ../genquotes/quotes/enclave.info.securityversion.json    tradewinds.us.test.attest.azure.net   false
+dotnet run ../genquotes/quotes/enclave.info.debug.json              sharedwus.us.test.attest.azure.net    false
+dotnet run ../genquotes/quotes/enclave.info.release.json            sharedwus.us.test.attest.azure.net    false
+dotnet run ../genquotes/quotes/enclave.info.prodid.json             sharedwus.us.test.attest.azure.net    false
+dotnet run ../genquotes/quotes/enclave.info.securityversion.json    sharedwus.us.test.attest.azure.net    false
 

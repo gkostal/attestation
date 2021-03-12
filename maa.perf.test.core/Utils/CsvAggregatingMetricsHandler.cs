b7e5e2370cf8ad2cc29c1a53c21b9f4e476e7d4c
Greg Kostal <gkostal@outlook.com>
2021-03-11 16:41:47 -0800

    WIP: #8

--- maa.perf.test.core/Utils/CsvAggregatingMetricsHandler.cs
+++ maa.perf.test.core/Utils/CsvAggregatingMetricsHandler.cs
@@ -68,7 +68,7 @@ namespace maa.perf.test.core.Utils
 
                     for (int j = 1; j < metrics.Count; j++)
                     {
-                        currentAggregation.Aggregate(metrics[i]);
+                        currentAggregation.Aggregate(metrics[j]);
                     }
 
                     var csvLine = string.Format("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}",

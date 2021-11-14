34d3ea2388bc11f8f1427fcd65e0e5c069263f19
Greg Kostal <gkostal@outlook.com>
2021-11-14 06:01:53 -0800

    Signing command line tool cc

--- maa.signing.tool/utils/JwtUtils.cs
+++ maa.signing.tool/utils/JwtUtils.cs
@@ -4,14 +4,20 @@ namespace maa.signing.tool.utils
     using System.Security.Cryptography;
     using System.Security.Cryptography.X509Certificates;
     using System.Text;
+    using Newtonsoft.Json;
 
     public class JwtUtils
     {
         public static string GenerateSignedPolicyJsonWebToken(string policy, RSA signingKey, X509Certificate2 signingCert)
         {
+
             if (!policy.StartsWith('"'))
             {
+                policy = policy.Replace("\n", @"\n");
+                policy = policy.Replace("\r", @"\r");
+                policy = policy.Replace("\"", "\\\"");
                 policy = "\"" + policy + "\"";
+                Tracer.TraceVerbose($"Updated policy to be signed = \n{policy}\n");
             }
             return GenerateSingleClaimJsonWebToken("AttestationPolicy", policy, signingKey, signingCert);
         }
@@ -21,6 +27,45 @@ namespace maa.signing.tool.utils
             var exportedCert = embeddedCertificate.Export(X509ContentType.Cert);
             string jwkToAdd = $"{{\"kty\":\"RSA\", \"x5c\":[\"{System.Convert.ToBase64String(exportedCert)}\"]}}";
             return GenerateSingleClaimJsonWebToken("maa-policyCertificate", jwkToAdd, signingKey, signingCert);
+        }
+
+        public static string FormatJwt(string raw)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] stringTokens = raw.Split(new char[] { '.', '\t', ' ', '\n', '\r' });
+
+            foreach (var tf in stringTokens)
+            {
+                try
+                {
+                    // We'll assume a base 64 URL encoded string that's a JSON structure.  If not, it's
+                    // OK to thrown an exception and move along.
+                    byte[] theBytes = Convert.FromBase64String(Pad(tf.Replace('-', '+').Replace('_', '/')));
+                    var obj = JsonConvert.DeserializeObject(System.Text.Encoding.UTF8.GetString(theBytes));
+                    if (obj != null)
+                    {
+                        sb.AppendFormat("{0}\n", obj.ToString());
+                    }
+                }
+                catch (Exception)
+                {
+                    //sb.AppendFormat("Non JSON Field ignored\n");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private static string Pad(string input)
+        {
+            var count = 3 - ((input.Length + 3) % 4);
+
+            if (count == 0)
+            {
+                return input;
+            }
+
+            return input + new string('=', count);
         }
 
         private static string GenerateSignedJsonWebToken(string jwtBody, RSA signingKey, X509Certificate2 signingCert)
